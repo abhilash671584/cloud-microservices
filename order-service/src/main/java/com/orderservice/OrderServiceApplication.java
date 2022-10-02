1445abfd934807ca539272f806ed238f35726546
FunnGuy <FunnGuy@funnyguy.com>
2022-10-02 23:27:36 +0530

    Added docker commands and two microsrvs and one disc server

--- order-service/src/main/java/com/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/orderservice/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.abp.productservice;
+package com.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ProductServiceApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
 }
